7ff17e906dec8912d147b2e642fdd7b5af16aea4
aritra-gaming-yt <78933353+aritra-gaming-yt@users.noreply.github.com>
2024-02-06 15:49:34 +0530

    owner.js

--- commands/owner.js
+++ commands/owner.js
@@ -18,7 +18,7 @@ module.exports = {
           iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157310253520662638/2443-iconperson.png?ex=651824aa&is=6516d32a&hm=0becc4a0fda01e5a02a63cf098db30c287e60a474f8d2da4ddeae7f47d98a5a3&',
           url: 'https://discord.gg/FUEHs7RCqz'
         })
-            .setDescription(`__**About me**__:\n\n ▶️ Hey there, Xenon this side. There's nothing to know about me am just a lazyass developer and a so called youtuber as a gamer. That's all... Yes you can contact me though the the Id mentioned in the about section of the bot. Thanks for reading.\n YouTube : ❤️ [Knighters™](${youtubeLink})\n`)
+            .setDescription(`__**About me**__:\n\n ▶️ Hey there, Xenon this side. There's nothing to know about me... am just a lazyass developer and a so called youtuber and a amateur gamer as well. That's all... and Yes you can contact me though the the Discord I'D mentioned in the about section of the bot and also by joining our community server (Link given below). Thanks for using our bot.\n Discord : [Knighters™](${youtubeLink})\n`)
             .setTimestamp();
       interaction.reply({ embeds: [embed] }).catch(e => {});
 
